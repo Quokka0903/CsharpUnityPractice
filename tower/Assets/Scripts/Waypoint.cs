31ffb4108005f3e2a02f7b3679d4d508a96424d7
0842956_이정규 <kue411@naver.com>
2023-03-03 17:42:38 +0900

    tower

---
+++ tower/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] bool isPlaceable;
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isPlaceable)
+            {
+                Instantiate(towerPrefab, transform.position, Quaternion.identity);
+                isPlaceable = false;
+            }
+        }
+    }
+}
